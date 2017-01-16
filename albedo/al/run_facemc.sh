5d05e34e3e0a835667f885b67c39aabeb6a2044f
Luke Kersting <lkersting@wisc.edu>
2017-01-16 10:30:09 -0600

    updated python scripts

--- albedo/al/run_facemc.sh
+++ albedo/al/run_facemc.sh
@@ -25,9 +25,13 @@ then
     THREADS="$1"
 fi
 
+
+ELEMENT="Al"
+ENERGY="0.005"
+
+ENERGY_EV=$(echo $ENERGY*1000000 |bc)
+ENERGY_EV=${ENERGY_EV%.*}
 NAME="ace"
-MAT="mat_ace.xml"
-INFO="sim_info.xml"
 
 echo -n "Enter the desired data type (1 = ACE, 2 = Native, 3 = Moment Preserving) > "
 read INPUT
@@ -35,22 +39,22 @@ if [ ${INPUT} -eq 1 ]
 then
     # Use ACE data
     NAME="ace"
-    MAT="mat_ace.xml"
-    INFO="sim_info.xml"
+    python mat.py -n ${ELEMENT} -t ${NAME}
+    python sim_info.py -e ${ENERGY} -c 1.0
     echo "Using ACE data!"
 elif [ ${INPUT} -eq 2 ]
 then
     # Use Native analog data
-    NAME="native"
-    MAT="mat_native.xml"
-    INFO="sim_info.xml"
+    NAME="linlin"
+    python mat.py -n ${ELEMENT} -t ${NAME}
+    python sim_info.py -e ${ENERGY} -c 1.0
     echo "Using Native analog data!"
 elif [ ${INPUT} -eq 3 ]
 then
     # Use Native Moment Preserving data
-    NAME="moments"
-    MAT="mat_native.xml"
-    INFO="sim_info_moments.xml"
+    NAME="linlin_moments"
+    python mat.py -n ${ELEMENT} -t "linlin"
+    python sim_info.py -e ${ENERGY} -c 0.9
     echo "Using Native Moment Preserving data!"
 else
     # Default to ACE data
@@ -58,18 +62,22 @@ else
 fi
 
 # .xml file paths.
+python ../est.py -e ${ENERGY}
+python source.py -e ${ENERGY}
+MAT="mat.xml"
+INFO="sim_info.xml"
 GEOM="geom.xml"
 SOURCE="source.xml"
 RSP="../rsp_fn.xml"
 EST="../est.xml"
-NAME="al_${NAME}"
+NAME="al_${NAME}_${ENERGY_EV}"
 
 TODAY=$(date +%Y-%m-%d)
 DIR="results/${TODAY}"
 mkdir -p $DIR
 
 echo "Running Facemc with ${THREADS} threads:"
-${FRENSIE}/bin/facemc --sim_info=${INFO} --geom_def=${GEOM} --mat_def=${MAT} --resp_def=$RSP --est_def=$EST --src_def=$SOURCE --cross_sec_dir=$CROSS_SECTION_XML_PATH --simulation_name=$NAME --threads=${THREADS} > ${DIR}/${NAME}.txt 2>&1
+${FRENSIE}/bin/facemc --sim_info=${INFO} --geom_def=${GEOM} --mat_def=${MAT} --resp_def=${RSP} --est_def=${EST} --src_def=${SOURCE} --cross_sec_dir=$CROSS_SECTION_XML_PATH --simulation_name=${NAME} --threads=${THREADS} > ${DIR}/${NAME}.txt 2>&1
 
 echo "Processing the results:"
 
