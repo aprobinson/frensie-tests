3e726a161e5ba056fcd4ccb7bc7d4d2064df42ef
Luke Kersting <lkersting@wisc.edu>
2017-01-27 12:30:56 -0600

    updated al and hanson test files.

---
+++ hanson/data_processor.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+##---------------------------------------------------------------------------##
+## FACEMC test result data processor
+##---------------------------------------------------------------------------##
+
+EXTRA_ARGS=$@
+TESTING_DIR="/home/lkersting/frensie/frensie-tests"
+
+if [ "$#" -ne 1 ];
+then
+    echo "The input file is required. $# arguments provided!"
+    echo "run:  ./data_processor.sh <file>"
+else
+    # Set file name
+    FILE=$1
+
+    H5="${FILE}.h5"
+
+    if [ -f $H5 ];
+    then
+        output="${FILE}_tranmission.txt"
+        # Extract the surface current data for transmission
+        ${TESTING_DIR}/edump.py -f $H5 -e 1 -i 6 -b Cosine > $output
+
+        output="${FILE}_reflection.txt"
+        # Extract the surface current data for reflection
+        ${TESTING_DIR}/edump.py -f $H5 -e 2 -i 4 -b Cosine > $output
+
+        output="${FILE}_cell_flux.txt"
+        # Extract the surface current data
+        ${TESTING_DIR}/edump.py -f $H5 -e 3 -i total -b Energy > $output
+    else
+       echo "File $H5 does not exist."
+    fi
+fi
---
+++ hanson/run_hanson_mpi.sh
@@ -0,0 +1,115 @@
+#!/bin/sh
+# This file is named run_facemc_mpi.sh
+#SBATCH --partition=univ2
+#SBATCH --time=0-20:00:00
+#SBATCH --nodes=5
+#SBATCH --ntasks-per-node=20
+#SBATCH --mem-per-cpu=4000
+
+##---------------------------------------------------------------------------##
+## ---------------------------- FACEMC test runner --------------------------##
+##---------------------------------------------------------------------------##
+## Validation runs comparing FRENSIE and MCNP.
+## The electron angular distribution for a thin gold foil of .0009658 cm.
+## The # of particles per steradian for scattering angle is found by dividing
+## the surface current by 2pi * ( \mu_{i} - \mu_{i-1} ) where \mu_{0} is the
+## lowest cosine bin (ie: -1). Surface current is needed so DagMC will be used.
+## The #/steradians can be changed to #/square degree by multiplying by
+## (pi/180)^2.
+## FRENSIE will be run with three variations.
+## 1. Using ACE data, which should match MCNP almost exactly.
+## 2. Using the Native data in analog mode, whcih uses a different interpolation
+## scheme than MCNP.
+## 3. Using Native data in moment preserving mode, which should give a less
+## acurate answer while decreasing run time.
+
+##---------------------------------------------------------------------------##
+## ------------------------------- COMMANDS ---------------------------------##
+##---------------------------------------------------------------------------##
+
+# Set cross_section.xml directory path.
+EXTRA_ARGS=$@
+CROSS_SECTION_XML_PATH=/home/ecmartin3/software/mcnpdata/
+FRENSIE=/home/lkersting/frensie
+
+INPUT="1"
+if [ "$#" -eq 1 ];
+then
+    # Set the file type (1 = ACE, 2 = Native, 3 = Moment Preserving)
+    INPUT="$1"
+fi
+
+# Changing variables
+THREADS="100"
+ELEMENT="Au"
+# Number of histories 1e6
+HISTORIES="1000000"
+
+ENERGY="15.7"
+NAME="ace"
+
+if [ ${INPUT} -eq 1 ]
+then
+    # Use ACE data
+    NAME="ace"
+    python sim_info.py -n ${HISTORIES} -c 1.0
+    python mat.py -n ${ELEMENT} -t ${NAME}
+    MAT="mat_ace.xml"
+    echo "Using ACE data!"
+elif [ ${INPUT} -eq 2 ]
+then
+    # Use Native analog data
+    NAME="native"
+    python sim_info.py -n ${HISTORIES} -c 1.0
+    python mat.py -n ${ELEMENT} -t "linlin"
+    MAT="mat.xml"
+    echo "Using Native analog data!"
+elif [ ${INPUT} -eq 3 ]
+then
+    # Use Native Moment Preserving data
+    NAME="moments"
+    python sim_info.py -n ${HISTORIES} -c 0.9
+    python mat.py -n ${ELEMENT} -t "linlin"
+    MAT="mat.xml"
+    echo "Using Native Moment Preserving data!"
+else
+    # Default to ACE data
+    python sim_info.py -n ${HISTORIES} -c 1.0
+    python mat.py -n ${ELEMENT} -t ${NAME}
+    MAT="mat_ace.xml"
+    echo "Input not valid, ACE data will be used!"
+fi
+
+# .xml file paths.
+python geom.py -t DagMC
+python est.py
+python source.py
+EST="est.xml"
+SOURCE="source.xml"
+INFO="sim_info.xml"
+GEOM="geom.xml"
+SOURCE="source.xml"
+RSP="../rsp_fn.xml"
+NAME="hanson_lin_${NAME}"
+
+# Make directory for the test results
+TODAY=$(date +%Y-%m-%d)
+DIR="results/${TODAY}"
+mkdir -p $DIR
+
+echo "Running Facemc Hanson (lin) test with ${THREADS} threads:"
+RUN="mpiexec -n ${THREADS} ${FRENSIE}/bin/facemc-mpi --sim_info=${INFO} --geom_def=${GEOM} --mat_def=${MAT} --resp_def=${RSP} --est_def=${EST} --src_def=${SOURCE} --cross_sec_dir=${CROSS_SECTION_XML_PATH} --simulation_name=${NAME}"
+echo ${RUN}
+${RUN} > ${DIR}/${NAME}.txt 2>&1
+
+echo "Moving the results:"
+
+# Move file to the test results folder
+H5=${NAME}.h5
+NEW_NAME="${DIR}/${H5}"
+NEW_RUN_INFO="${DIR}/continue_run_${NAME}.xml"
+
+mv ${H5} ${NEW_NAME}
+mv continue_run.xml ${NEW_RUN_INFO}
+
+echo "Results will be in ./${DIR}"
